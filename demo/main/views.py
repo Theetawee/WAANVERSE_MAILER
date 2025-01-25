a03e173473275a595e3de51cc65d74a39f1189b4
Pattawee Drake Khaotungkulmethee <140291119+Theetawee@users.noreply.github.com>
2025-01-25 05:44:28 +0300

    save

--- demo/main/views.py
+++ demo/main/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+from waanverse_mailer.email_service import EmailService
+
 # Create your views here.
 
 
 def index(request):
+    email_service = EmailService()
+    email_service.send_email(
+        recipient_list="test@example.com",
+        subject="Test Subject",
+        template_name="single_email",
+        context={"name": "John"},
+    )
     return render(request, "index.html")
